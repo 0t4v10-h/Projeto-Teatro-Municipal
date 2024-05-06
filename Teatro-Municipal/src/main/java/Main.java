bf398e8e49bcf7114da08489ac47d7e33f465869
Samuel Costa <samuel@samuel>
2024-05-06 17:53:39 -0300

    teste atualizando o Main

---
+++ Teatro-Municipal/src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static void eventosDisponiveis() {
+        Evento e1 = new Evento(1, "Espetaculo Rei Leão", "05/06/2024", "18:00", "A apresentação em formato musical" +
+                "fala sobre familia e superação");
+    }
+
+}
